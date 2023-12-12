d5d7cc9fbeb7cdcc7c3ab06f447fb5fbf0b66e53
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-12-11 19:46:21 -0600

    started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,12 @@
+const {Chat, Message, User} = require('../models');
+const { signToken, AuthenticationError} = require('../utils/auth');
+const stripe = require('stripe')("sk_test_51JQ4Q");
 
+const resolvers = {
+    Query: {
+        categories: async () => {
+            return await Category.find();
+        },
+        prodec
+    }
+}
