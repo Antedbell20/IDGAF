5dd4e214afbf5f30f593133cb88c098e6981bdf9
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2023-12-12 15:05:15 -0600

    Sinup and auth,mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,38 @@
 import { gql } from '@apollo/client';
 
-const createMutation = (name, args, fields) => gql`
-  mutation ${name}(${args}) {
-    ${name}(${args.split(', ').map(arg => arg.split(':')[0]).join(', ')}) {
-      ${fields}
+// const createMutation = (name, args, fields) => gql`
+//   mutation ${name}(${args}) {
+//     ${name}(${args.split(', ').map(arg => arg.split(':')[0]).join(', ')}) {
+//       ${fields}
+//     }
+//   }
+// `;
+
+// export const SIGNUP = createMutation('signup', '$username: String!, $email: String!, $password: String!', '_id\nusername\nemail');
+// export const LOGIN = createMutation('login', '$email: String!, $password: String!', '_id\nusername\nemail');
+// export const ADD_FRIEND = createMutation('addFriend', '$userId: ID!', '_id\nuser {\n_id\nusername\nemail\n}');
+// export const CREATE_GROUP = createMutation('createGroup', '$name: String!, $memberIds: [ID!]!', '_id\nname\nmembers {\n_id\nusername\nemail\n}');
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
 
-export const SIGNUP = createMutation('signup', '$username: String!, $email: String!, $password: String!', '_id\nusername\nemail');
-export const LOGIN = createMutation('login', '$email: String!, $password: String!', '_id\nusername\nemail');
-export const ADD_FRIEND = createMutation('addFriend', '$userId: ID!', '_id\nuser {\n_id\nusername\nemail\n}');
-export const CREATE_GROUP = createMutation('createGroup', '$name: String!, $memberIds: [ID!]!', '_id\nname\nmembers {\n_id\nusername\nemail\n}');
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
