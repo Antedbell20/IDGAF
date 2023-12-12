479cea7d47c49793f2590143c3042cc0116e26e1
Ezekiel <ezekieljamolin186@gmail.com>
2023-12-11 18:31:47 -0800

    made text area for textin in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 
-function Home() {
+const Home = () => {
+  const [formData, setFormData] = useState({ message: '', });
+
+  const { message, } = formData;
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  };
+
   return (
     <div className='home-container'>
       <div className='left'>
         <button>Add Friend</button>
       </div>
       <div className='right'>
-        
+        <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="message"></label>
+          <input
+            type="text"
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleChange}/>
+        </div>
+        </form>
       </div>
     </div>
   )
