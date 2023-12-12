ceb7dfe7845d8ace3d19e72af9faeb0bee2bedb4
Chris San <christianes322@gmail.com>
2023-12-11 20:52:37 -0600

    cleaned up connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/IDGAF');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/IDGAF', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error(`MongoDB connection error: ${err}`));
 
-const db = mongoose.connection;
-
-// Event listener for successful connection
-db.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-// Event listener for connection error
-db.on('error', (err) => {
-  console.error(`MongoDB connection error: ${err}`);
-});
-
-// Event listener for disconnected
-db.on('disconnected', () => {
-  console.log('MongoDB disconnected');
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB disconnected');
+    process.exit(0);
+  });
 });
 
 module.exports = db;
